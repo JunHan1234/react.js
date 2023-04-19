0c3c6aaea05b80d5b18325faf31bdd6404dfd984
Han1234 <a@a.com>
2023-04-19 15:25:39 +0900

    1

---
+++ src/1.basic/1.Button.js
@@ -0,0 +1,6 @@
+//Button module을 export한다.
+export default function Button() {
+    return (
+        <button>I'm a button.</button>
+    )
+}
---
+++ src/1.basic/App.js
@@ -0,0 +1,10 @@
+//main 역할의 파일명을 보통 App.js로 설정한다.
+//Button을 가져온다.
+import Button from './1.Button'
+
+export default function App() {
+    return (
+        //react에서 component function을 call하는 문법
+        <Button/>
+    )
+}
